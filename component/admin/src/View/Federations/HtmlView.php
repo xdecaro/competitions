b25aa8db515794305d4acb8accc126a1a8df9890
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-06 03:41:54 +0200

    Load Competitions admin assets in Federations

--- component/admin/src/View/Federations/HtmlView.php
+++ component/admin/src/View/Federations/HtmlView.php
@@ -7,6 +7,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use Xdecaro\Component\Decarodcl\Administrator\Helper\UiHelper;
 
 final class HtmlView extends BaseHtmlView
 {
@@ -23,6 +24,7 @@ final class HtmlView extends BaseHtmlView
             throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
         }
 
+        UiHelper::loadAssets();
         $this->items = $this->get('Items');
         $this->pagination = $this->get('Pagination');
         $this->state = $this->get('State');
