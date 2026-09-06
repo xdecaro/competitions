38e9526c20f9878ec642ac517eba420b92a2c8a0
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-07 01:45:28 +0200

    Load global live sync assets

--- component/admin/src/Helper/UiHelper.php
+++ component/admin/src/Helper/UiHelper.php
@@ -4,6 +4,8 @@ namespace Xdecaro\Component\Decarodcl\Administrator\Helper;
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Session\Session;
 
 final class UiHelper
 {
@@ -13,12 +15,34 @@ final class UiHelper
 
         $document = Factory::getApplication()->getDocument();
         $wa = $document->getWebAssetManager();
-        $assetName = 'com_decarodcl.admin.runtime';
+        $styleName = 'com_decarodcl.admin.runtime';
+        $scriptName = 'com_decarodcl.live-sync.runtime';
 
-        if (!$wa->assetExists('style', $assetName)) {
-            $wa->registerStyle($assetName, 'com_decarodcl/admin.css', ['version' => '0.9.1']);
+        if (!$wa->assetExists('style', $styleName)) {
+            $wa->registerStyle($styleName, 'com_decarodcl/admin.css', ['version' => '0.10.0']);
         }
 
-        $wa->useStyle($assetName);
+        if (!$wa->assetExists('script', $scriptName)) {
+            $wa->registerScript(
+                $scriptName,
+                'com_decarodcl/live-sync.js',
+                ['version' => '0.10.0'],
+                ['defer' => true]
+            );
+        }
+
+        $document->addScriptOptions('com_decarodcl.liveSync', [
+            'endpoint' => 'index.php?option=com_decarodcl&task=sync.poll&format=json',
+            'token' => Session::getFormToken(),
+            'interval' => 5000,
+            'strings' => [
+                'presence' => Text::_('COM_DECARODCL_LIVE_PRESENCE'),
+                'conflict' => Text::_('COM_DECARODCL_LIVE_CONFLICT'),
+                'reload' => Text::_('COM_DECARODCL_LIVE_RELOAD'),
+            ],
+        ]);
+
+        $wa->useStyle($styleName);
+        $wa->useScript($scriptName);
     }
 }
