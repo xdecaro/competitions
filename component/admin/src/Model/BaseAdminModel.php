c19c71c4ee57f71e966d393b70106e345641cfa0
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-07 01:42:23 +0200

    Add shared optimistic locking and change logging

---
+++ component/admin/src/Model/BaseAdminModel.php
@@ -0,0 +1,85 @@
+<?php
+namespace Xdecaro\Component\Decarodcl\Administrator\Model;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\AdminModel;
+use Xdecaro\Component\Decarodcl\Administrator\Helper\LanguageHelper;
+use Xdecaro\Component\Decarodcl\Administrator\Helper\LiveSyncHelper;
+
+abstract class BaseAdminModel extends AdminModel
+{
+    public function save($data): bool
+    {
+        $entity = LiveSyncHelper::entityFromModelName($this->getName());
+        $id = (int) ($data['id'] ?? 0);
+        $isNew = $id <= 0;
+
+        if (!$isNew && $entity !== '') {
+            $expectedModified = trim((string) Factory::getApplication()->input->post->getString('dcl_expected_modified', ''));
+
+            if ($expectedModified !== '') {
+                $currentModified = LiveSyncHelper::currentModified($this->getDatabase(), $entity, $id);
+
+                if ($currentModified !== null && $currentModified !== '' && $currentModified !== $expectedModified) {
+                    LanguageHelper::load();
+                    $this->setError(Text::_('COM_DECARODCL_ERROR_LIVE_CONFLICT'));
+
+                    return false;
+                }
+            }
+        }
+
+        if (!parent::save($data)) {
+            return false;
+        }
+
+        $savedId = (int) $this->getState($this->getName() . '.id');
+
+        if ($savedId > 0 && $entity !== '') {
+            LiveSyncHelper::record($this->getDatabase(), $entity, $savedId, $isNew ? 'create' : 'update');
+        }
+
+        return true;
+    }
+
+    public function publish(&$pks, $value = 1): bool
+    {
+        $ids = array_values(array_filter(array_map('intval', (array) $pks)));
+
+        if (!parent::publish($pks, $value)) {
+            return false;
+        }
+
+        $entity = LiveSyncHelper::entityFromModelName($this->getName());
+
+        if ($entity !== '') {
+            foreach ($ids as $id) {
+                LiveSyncHelper::record($this->getDatabase(), $entity, $id, 'state');
+            }
+        }
+
+        return true;
+    }
+
+    public function delete(&$pks): bool
+    {
+        $ids = array_values(array_filter(array_map('intval', (array) $pks)));
+
+        if (!parent::delete($pks)) {
+            return false;
+        }
+
+        $entity = LiveSyncHelper::entityFromModelName($this->getName());
+
+        if ($entity !== '') {
+            foreach ($ids as $id) {
+                LiveSyncHelper::record($this->getDatabase(), $entity, $id, 'delete');
+            }
+        }
+
+        return true;
+    }
+}
