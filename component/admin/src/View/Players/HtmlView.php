f4f1746693fa77d0a566b0b115d2cae6403b5e9c
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-16 23:40:06 +0200

    feat: add approval actions to players toolbar

--- component/admin/src/View/Players/HtmlView.php
+++ component/admin/src/View/Players/HtmlView.php
@@ -46,6 +46,27 @@ final class HtmlView extends BaseHtmlView
         }
 
         if ($user->authorise('core.edit.state', 'com_xdecarocompetitions')) {
+            ToolbarHelper::custom(
+                'players.approve',
+                'checkmark',
+                'checkmark',
+                Text::_('COM_XDECAROCOMPETITIONS_TOOLBAR_APPROVE'),
+                true
+            );
+            ToolbarHelper::custom(
+                'players.pending',
+                'clock',
+                'clock',
+                Text::_('COM_XDECAROCOMPETITIONS_TOOLBAR_PENDING'),
+                true
+            );
+            ToolbarHelper::custom(
+                'players.reject',
+                'cancel',
+                'cancel',
+                Text::_('COM_XDECAROCOMPETITIONS_TOOLBAR_REJECT'),
+                true
+            );
             ToolbarHelper::publish('players.publish', 'JTOOLBAR_PUBLISH', true);
             ToolbarHelper::unpublish('players.unpublish', 'JTOOLBAR_UNPUBLISH', true);
         }
