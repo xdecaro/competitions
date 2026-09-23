490054a4e0f07a013cce3f9e3b9a00adfe42eb9b
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-23 15:31:03 +0200

    Render live-sync lock fields in edit form

--- component/admin/tmpl/organization/edit.php
+++ component/admin/tmpl/organization/edit.php
@@ -5,6 +5,8 @@ HTMLHelper::_('behavior.formvalidator');
 ?>
 <form action="index.php?option=com_xdecarocompetitions&layout=edit&id=<?= (int) $this->item->id; ?>" method="post" name="adminForm" id="organization-form" class="form-validate competitions-admin">
     <div class="card"><div class="card-body"><?= $this->form->renderFieldset('details'); ?></div></div>
+    <?= $this->form->getInput('id'); ?>
+    <?= $this->form->getInput('modified'); ?>
     <input type="hidden" name="task" value="">
     <?= HTMLHelper::_('form.token'); ?>
 </form>
