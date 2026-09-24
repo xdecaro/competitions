9571a49c5e626f8d63a16e77c8f1b076ca2f6641
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-24 02:52:54 +0200

    Bump runtime asset version to 1.5.12

--- component/admin/src/Helper/UiHelper.php
+++ component/admin/src/Helper/UiHelper.php
@@ -9,7 +9,7 @@ use Joomla\CMS\Session\Session;
 
 final class UiHelper
 {
-    private const VERSION = '1.5.11';
+    private const VERSION = '1.5.12';
 
     public static function loadTeamImportAsset(): void
     {
