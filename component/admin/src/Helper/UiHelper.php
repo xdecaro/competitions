4d8c370f7f1bf085b43e17f02864f37351bbe780
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-25 22:46:38 +0200

    Bump Competitions runtime asset version

--- component/admin/src/Helper/UiHelper.php
+++ component/admin/src/Helper/UiHelper.php
@@ -9,7 +9,7 @@ use Joomla\CMS\Session\Session;
 
 final class UiHelper
 {
-    private const VERSION = '1.5.20';
+    private const VERSION = '1.5.21';
 
     public static function loadAssets(): void
     {
