f20063ac0ccb56cd2ed69fc645ee4056c5d8e3cb
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-25 02:26:02 +0200

    Bump Competitions runtime assets to 1.5.17

--- component/admin/src/Helper/UiHelper.php
+++ component/admin/src/Helper/UiHelper.php
@@ -9,7 +9,7 @@ use Joomla\CMS\Session\Session;
 
 final class UiHelper
 {
-    private const VERSION = '1.5.16';
+    private const VERSION = '1.5.17';
 
     public static function loadAssets(): void
     {
